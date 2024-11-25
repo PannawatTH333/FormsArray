7389b52c692b9c6df6759239251522c50ec0d67c
PannawatTH333 <Plamthailand2546@gmail.com>
2024-11-25 12:06:51 +0700

    Add project files.

---
+++ WindowsFormsArray/Form1.Designer.cs
@@ -0,0 +1,197 @@
+﻿namespace WindowsFormsArray
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.button3 = new System.Windows.Forms.Button();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.button4 = new System.Windows.Forms.Button();
+            this.button5 = new System.Windows.Forms.Button();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.checkedListBox1 = new System.Windows.Forms.CheckedListBox();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(41, 21);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(147, 67);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Array";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(41, 102);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(147, 67);
+            this.button2.TabIndex = 1;
+            this.button2.Text = "Array2";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(304, 35);
+            this.textBox1.Multiline = true;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(195, 406);
+            this.textBox1.TabIndex = 2;
+            // 
+            // label1
+            // 
+            this.label1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.label1.Location = new System.Drawing.Point(609, 35);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(179, 406);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "label1";
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(505, 81);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(98, 66);
+            this.button3.TabIndex = 4;
+            this.button3.Text = "GetData";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(273, 14);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(61, 13);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "พิมพ์รายชื่อ";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(585, 14);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(74, 13);
+            this.label3.TabIndex = 6;
+            this.label3.Text = "รายชื่อที่บันทึก";
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(505, 185);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(98, 66);
+            this.button4.TabIndex = 7;
+            this.button4.Text = "GetNumber";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // button5
+            // 
+            this.button5.Location = new System.Drawing.Point(41, 185);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(147, 67);
+            this.button5.TabIndex = 8;
+            this.button5.Text = "Array 2 มิติ";
+            this.button5.UseVisualStyleBackColor = true;
+            this.button5.Click += new System.EventHandler(this.button5_Click);
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Location = new System.Drawing.Point(41, 275);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(121, 21);
+            this.comboBox1.TabIndex = 9;
+            this.comboBox1.SelectedIndexChanged += new System.EventHandler(this.comboBox1_SelectedIndexChanged);
+            // 
+            // listBox1
+            // 
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.Location = new System.Drawing.Point(41, 321);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(120, 95);
+            this.listBox1.TabIndex = 10;
+            this.listBox1.SelectedIndexChanged += new System.EventHandler(this.listBox1_SelectedIndexChanged);
+            // 
+            // checkedListBox1
+            // 
+            this.checkedListBox1.FormattingEnabled = true;
+            this.checkedListBox1.Location = new System.Drawing.Point(178, 321);
+            this.checkedListBox1.Name = "checkedListBox1";
+            this.checkedListBox1.Size = new System.Drawing.Size(120, 94);
+            this.checkedListBox1.TabIndex = 11;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.checkedListBox1);
+            this.Controls.Add(this.listBox1);
+            this.Controls.Add(this.comboBox1);
+            this.Controls.Add(this.button5);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "ArrayForm";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Button button5;
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.CheckedListBox checkedListBox1;
+    }
+}
+
---
+++ WindowsFormsArray/Form1.cs
@@ -0,0 +1,130 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsArray
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            string[] student = new string[5];
+            student[0] = "ต้น";
+            student[1] = "มือ";
+            student[2] = "แขน";
+            student[3] = "อาร์ม";
+            student[4] = "ปราง";
+
+            string test = "";
+            int n=student.Length;
+            for (int i = 0; i < n; i++)
+            {
+                test += student[i] + Environment.NewLine;
+            }
+            MessageBox.Show(test);
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            string[] Fruit = { "ส้ม", "มะม่วง", "มะขาม", "มะยม", "ส้มโอ" };
+            string myFruit = "";
+            foreach (string str in Fruit)
+            {
+                myFruit += str + ", ";
+            }
+            MessageBox.Show(myFruit, "ผลไม้ของไผ๋");
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            string[] Fruit = { "ส้ม", "มะม่วง", "มะขาม", "มะยม", "ส้มโอ" };
+            comboBox1.Items.AddRange(Fruit);
+            listBox1.Items.AddRange(Fruit);
+            checkedListBox1.Items.AddRange(Fruit);
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            string[] data = textBox1.Lines;  //นำข้อมูลแต่ละบรรทัดไปใส่
+            string result = "";
+            foreach (string str in data)
+            {
+                if (str != "")
+                {
+                    result += str + Environment.NewLine;
+                }
+                
+            }
+            label1.Text = result;
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            string[] data = textBox1.Lines;
+            string result = "";
+            int sum = 0;
+            foreach (var str in data)
+            {
+
+                
+                int x = 0;
+                if (int.TryParse(str,out x))
+                {
+                    result += x.ToString() + Environment.NewLine;
+                    sum += x;
+                }
+            }
+            label1.Text = result;
+            MessageBox.Show(sum.ToString(), "ผลรวมของตัวเลข");
+        }
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+            string[,] student = new string[3,2];
+            student[0, 0] = "ต้น";
+            student[0, 1] = "อุดร";
+            student[1, 0] = "ขาม";
+            student[1, 1] = "หนองแสง";
+            student[2, 0] = "เอ็ก";
+            student[2, 1] = "หนองคาย";
+            string ResultData = "";
+            int m = student.GetLength(0);
+            int n = student.GetLength(1);
+            for (int i = 0; i < m; i++)
+            {
+                for (int j = 0; j < n; j++)
+                {
+                    ResultData += student[i, j] + "_";
+                }
+                ResultData += Environment.NewLine;
+            }
+            MessageBox.Show(ResultData, "แสดงข้อมูล Array 2มิติ");
+        }
+
+        private void comboBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            string str = comboBox1.SelectedIndex.ToString()  + "_";
+            str += comboBox1.SelectedItem;
+            MessageBox.Show(str);
+        }
+
+        private void listBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            string str = listBox1.SelectedIndex.ToString() + "_";
+            str += listBox1.SelectedItem;
+            MessageBox.Show(str);
+        }
+    }
+}
